084333642859787751bb30193621e5ea52e4104a
hyunjeong <cchloe0927@gmail.com>
2022-12-16 15:24:20 +0900

    style : setting layout

---
+++ React/src/shared/layout/Layout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Layout.module.css";
+
+const Layout = (props) => {
+  return <div className={classes.layout}>{props.children}</div>;
+};
+
+export default Layout;
