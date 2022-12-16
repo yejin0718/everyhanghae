697efe5e50f7d0367be7e1a72ba1f0c3bf0d20bc
hyunjeong <cchloe0927@gmail.com>
2022-12-16 17:36:16 +0900

    style : header

--- React/src/shared/layout/Header.jsx
+++ React/src/shared/layout/Header.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import classes from "./Header.module.css";
 //component
 import Button from "../../components/elements/Button";
+import { Outlet } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className={classes.header}>
       <h2>애브리항해</h2>
       <Button>로그인</Button>
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 };
