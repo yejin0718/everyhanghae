67ec995a797601668558efd12ee38e335ff789b2
hyunjeong <cchloe0927@gmail.com>
2022-12-16 16:04:19 +0900

    feat : setting router

--- React/src/shared/Router.js
+++ React/src/shared/Router.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "../pages/Main";
+import Header from "./layout/Header";
 import Layout from "./layout/Layout";
-// import Header from "../components/layout/Header";
 import Footer from "./layout/Footer";
+import Main from "../pages/Main";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Layout>
         <Routes>
           <Route path="/" element={<Main />} />
---
+++ React/src/shared/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={classes.header}>
+      <span>Team Todo List</span>
+    </div>
+  );
+};
+
+export default Header;
