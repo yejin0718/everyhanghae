8dadbb4edc28894a8671cb1e74a098fb6ae56e8c
Yanjoo <hanjub3@gmail.com>
2022-12-16 14:44:56 +0900

    spring initial commit

---
+++ Spring/everyhanghae-backend/src/main/java/com/everyhanghae/EveryhanghaeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.everyhanghae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryhanghaeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryhanghaeBackendApplication.class, args);
+	}
+
+}
---
+++ Spring/everyhanghae-backend/src/test/java/com/everyhanghae/EveryhanghaeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everyhanghae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryhanghaeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
