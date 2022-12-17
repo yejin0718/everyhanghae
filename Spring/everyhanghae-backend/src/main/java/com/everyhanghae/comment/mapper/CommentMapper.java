c146a687d053339ff34df65613e309259d1ab755
김예진 <a89557400@gmail.com>
2022-12-17 21:46:48 +0900

    refactor : 댓글 작성 API 수정

--- Spring/everyhanghae-backend/src/main/java/com/everyhanghae/comment/mapper/CommentMapper.java
+++ Spring/everyhanghae-backend/src/main/java/com/everyhanghae/comment/mapper/CommentMapper.java
@@ -9,15 +9,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class CommentMapper {
 
-    public CommentResponseDto toResponseComment(Long id, Comment comment) {
-        return CommentResponseDto.builder()
-                .boardId(id)
-                .username(comment.getWriter())
-                .comment(comment.getContent())
-                .createdAt(comment.getCreatedAt())
-                .build();
-    }
-
     public Comment toDepthZeroComment(Board board, CommentRequestDto requestDto){
         return Comment.builder()
                 .content(requestDto.getComment())
