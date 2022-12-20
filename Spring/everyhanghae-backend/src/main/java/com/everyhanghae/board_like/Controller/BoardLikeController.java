880a0a34cc10a1c190d477007a03e2691782068e
yjk <djwhs415@gmail.com>
2022-12-20 11:11:01 +0900

    feat : 게시글 좋아요 API 회원 검증 로직 추가

--- Spring/everyhanghae-backend/src/main/java/com/everyhanghae/board_like/Controller/BoardLikeController.java
+++ Spring/everyhanghae-backend/src/main/java/com/everyhanghae/board_like/Controller/BoardLikeController.java
@@ -3,11 +3,15 @@ package com.everyhanghae.board_like.Controller;
 
 import com.everyhanghae.board_like.Service.BoardLikeService;
 import com.everyhanghae.common.response.Response;
+import com.everyhanghae.user.jwt.JwtService;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static com.everyhanghae.common.response.ResponseMessage.INSERT_LIKE_SUCCESS_MSG;
 import static com.everyhanghae.common.response.ResponseMessage.DELETTE_LIKE_SUCCESS_MSG;
@@ -18,11 +22,16 @@ import static com.everyhanghae.common.response.ResponseMessage.DELETTE_LIKE_SUCC
 public class BoardLikeController {
     private final BoardLikeService boardLikeService;
 
+    private final JwtService jwtService;
+
     /* 좋아요 등록, 취소 */
     @PostMapping("/{id}/like")
-    public Response boardLike(@PathVariable Long id){
+    public Response boardLike(@PathVariable Long id, HttpServletRequest servletRequest){
 
-        boolean likeboolean = boardLikeService.boardLike(id);
+        Claims claims = jwtService.getTokenClaim(servletRequest);
+        String email = claims.getSubject();
+
+        boolean likeboolean = boardLikeService.boardLike(id,email);
         if (likeboolean){ // 좋아요 등록
             return new Response(INSERT_LIKE_SUCCESS_MSG);
         }else { // 좋아요 취소
