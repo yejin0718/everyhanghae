83212d2f7b9b029d49ea144469effc9447ae7354
seungit <minsg3669@gmail.com>
2022-12-20 22:18:54 +0900

    Refactor : 로그인 response 값 내 데이터 추가

--- Spring/everyhanghae-backend/src/main/java/com/everyhanghae/user/controller/UserController.java
+++ Spring/everyhanghae-backend/src/main/java/com/everyhanghae/user/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.everyhanghae.user.controller;
 
+import com.everyhanghae.common.response.DataResponse;
 import com.everyhanghae.common.response.Response;
 import com.everyhanghae.user.dto.RequestCreateUser;
 import com.everyhanghae.user.dto.RequestLoginUser;
+import com.everyhanghae.user.dto.ResponseInfoUser;
 import com.everyhanghae.user.service.UserService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import static com.everyhanghae.common.response.ResponseMessage.CREATE_USER_SUCCESS_MSG;
 import static com.everyhanghae.common.response.ResponseMessage.LOGIN_USER_SUCCESS_MSG;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -26,8 +36,9 @@ public class UserController {
 
     @ResponseBody
     @PostMapping("/login")
-    public Response login(@RequestBody RequestLoginUser requestLoginUser, HttpServletResponse httpServletResponse) {
-        userService.login(requestLoginUser, httpServletResponse);
-        return new Response(LOGIN_USER_SUCCESS_MSG);
+    public DataResponse<ResponseInfoUser> login(@RequestBody RequestLoginUser requestLoginUser, HttpServletResponse httpServletResponse) {
+        ResponseInfoUser responseInfoUser = userService.login(requestLoginUser,httpServletResponse);
+        return new DataResponse<>(LOGIN_USER_SUCCESS_MSG, responseInfoUser);
     }
+
 }
