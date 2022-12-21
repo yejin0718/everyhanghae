5ef93d754f287d43472aef1d6add4929f35c10dd
yanJuicy <hanjub3@gmail.com>
2022-12-21 20:47:36 +0900

    refactor: ServletRequest -> UserDetails로 사용자 인증 데이터를 가지는 객체 변경

--- Spring/everyhanghae-backend/src/main/java/com/everyhanghae/domain/board_like/Controller/BoardLikeController.java
+++ Spring/everyhanghae-backend/src/main/java/com/everyhanghae/domain/board_like/Controller/BoardLikeController.java
@@ -1,20 +1,17 @@
 package com.everyhanghae.domain.board_like.Controller;
 
-
 import com.everyhanghae.domain.board_like.Service.BoardLikeService;
+import com.everyhanghae.security.UserDetailsImpl;
 import com.everyhanghae.shared.response.Response;
-import com.everyhanghae.security.jwt.service.JwtService;
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
-import static com.everyhanghae.shared.response.ResponseMessage.INSERT_LIKE_SUCCESS_MSG;
 import static com.everyhanghae.shared.response.ResponseMessage.DELETTE_LIKE_SUCCESS_MSG;
+import static com.everyhanghae.shared.response.ResponseMessage.INSERT_LIKE_SUCCESS_MSG;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,14 +19,10 @@ import static com.everyhanghae.shared.response.ResponseMessage.DELETTE_LIKE_SUCC
 public class BoardLikeController {
     private final BoardLikeService boardLikeService;
 
-    private final JwtService jwtService;
-
     /* 좋아요 등록, 취소 */
     @PostMapping("/{id}/like")
-    public Response boardLike(@PathVariable Long id, HttpServletRequest servletRequest){
-
-        Claims claims = jwtService.getTokenClaim(servletRequest);
-        String email = claims.getSubject();
+    public Response boardLike(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        String email = userDetails.getUsername();
 
         boolean likeboolean = boardLikeService.boardLike(id,email);
         if (likeboolean){ // 좋아요 등록
@@ -37,7 +30,6 @@ public class BoardLikeController {
         }else { // 좋아요 취소
             return new Response(DELETTE_LIKE_SUCCESS_MSG);
         }
-
     }
 
 }
