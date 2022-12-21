b503edd80e480586fd77642f8aa82b6453e36e7a
yanJuicy <hanjub3@gmail.com>
2022-12-21 18:22:14 +0900

    feat: SecurityConfig에 Filter, EntiryPoint 등록

--- Spring/everyhanghae-backend/src/main/java/com/everyhanghae/security/config/SecurityConfig.java
+++ Spring/everyhanghae-backend/src/main/java/com/everyhanghae/security/config/SecurityConfig.java
@@ -1,18 +1,31 @@
 package com.everyhanghae.security.config;
 
+import com.everyhanghae.security.exception.CustomAuthenticationEntryPoint;
+import com.everyhanghae.security.jwt.filter.JwtAuthFilter;
+import com.everyhanghae.security.jwt.service.JwtService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import lombok.RequiredArgsConstructor;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+	private final JwtService jwtService;
+
+	@Bean
+	public AuthenticationEntryPoint authenticationEntryPoint() {
+		return new CustomAuthenticationEntryPoint();
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,13 +38,19 @@ public class SecurityConfig {
 			.disable();
 
 		http.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+			.sessionCreationPolicy(STATELESS);
 
 		http.authorizeRequests()
-			.antMatchers("/api/**")
+			.antMatchers("/api/users/**")
+				.permitAll()
+			.antMatchers(GET, "/api/boards/**")
 				.permitAll()
 			.anyRequest()
 				.authenticated();
+
+		http.addFilterBefore(new JwtAuthFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
+		http.exceptionHandling()
+				.authenticationEntryPoint(authenticationEntryPoint());
 
 		return http.build();
 	}
